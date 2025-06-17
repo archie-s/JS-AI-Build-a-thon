004c7e8c8bc5d00e23f50c1f03e840dfceb0136a
archie-s <achiengjoyce2004@gmail.com>
2025-06-17 15:17:11 +0300

    Working with GitHub Models and AI Toolkit

--- sample.js
+++ sample.js
@@ -1,14 +1,14 @@
 import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
 import { AzureKeyCredential } from "@azure/core-auth";
+import fs from "fs";
 
 const token = process.env["GITHUB_TOKEN"];
 const endpoint = "https://models.github.ai/inference";
-const model = "deepseek/DeepSeek-R1-0528";
-import fs from "fs";
+const model = "openai/gpt-4.1";
 
 export async function main() {
 
-  const imagePath = "./contoso_layout_sketch.jpg"; 
+  const imagePath = "./contoso_layout_sketch.jpg";
   const imageBuffer = fs.readFileSync(imagePath);
   const imageBase64 = imageBuffer.toString("base64");
 
@@ -29,13 +29,14 @@ export async function main() {
           ]
         }
       ],
-      max_tokens: 1000,
+      temperature: 1.0,
+      top_p: 1.0,
       model: model
     }
   });
 
   if (isUnexpected(response)) {
-    throw response.body.error || response.body || response;
+    throw response.body.error;
   }
 
   console.log(response.body.choices[0].message.content);
